96f7c389cb0ae5ccb6abd921ac5b2be5779c9580
timakin <timaki.st@gmail.com>
2016-03-31 01:22:35 +0900

    init

---
+++ lib/itamae/plugin/resource/npm.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/npm/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Npm
+        # Your code goes here...
+      end
+    end
+  end
+end
